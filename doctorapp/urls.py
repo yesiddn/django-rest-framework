9c695ba0e84a49461c686052374d8d160986abb7
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-22 23:46:55 -0500

    Add URL routes for listing and creating patients

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from patients import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/patients/', views.list_patients),
+    path('api/patients/create', views.create_patient),
 ]

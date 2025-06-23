73453c2f6adae6690cfdf3d89cbb61fc4969f41b
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-22 23:46:38 -0500

    Add initial models, serializers, views, and migrations for  Bookings management

---
+++ bookings/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from doctors.models import Doctor
+from patients.models import Patient
+
+class Appointment(models.Model):
+  patient = models.ForeignKey(
+    Patient, related_name='appointments', on_delete=models.CASCADE
+  )
+  doctor = models.ForeignKey(
+    Doctor, related_name='appointments', on_delete=models.CASCADE
+  )
+  appointment_date = models.DateField()
+  appointment_time = models.TimeField()
+  notes = models.TextField(blank=True, null=True)
+  # status = models.CharField(
+  #   max_length=20,
+  #   choices=[
+  #     ('scheduled', 'Scheduled'),
+  #     ('completed', 'Completed'),
+  #     ('cancelled', 'Cancelled'),
+  #   ],
+  #   default='scheduled'
+  # )
+  status = models.CharField(max_length=10)
+
+class MedicalNote(models.Model):
+  appointment = models.ForeignKey(
+    Appointment, related_name='medical_notes', on_delete=models.CASCADE
+  )
+  note = models.TextField()
+  date = models.DateField()

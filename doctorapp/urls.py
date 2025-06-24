953f3c8e26fe32cd2449cb765e90087d0996a88c
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-24 18:10:34 -0500

    Add URL routing for doctor views in the main URL configuration

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from patients import urls
+from doctors import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(urls)),
+    path('api/doctors/', include(urls)),
 ]

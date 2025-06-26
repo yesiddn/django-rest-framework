cde75db0b41e55dd3be46fd453dab72e94d11c06
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-25 21:28:09 -0500

    Add REST framework and Spectacular settings for API documentation

--- doctorapp/settings.py
+++ doctorapp/settings.py
@@ -31,16 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'patients',
-    'doctors',
-    'bookings',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "patients",
+    "doctors",
+    "bookings",
+    "docs",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Doctor App API",
+    "DESCRIPTION": "API documentation for the Doctor App",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+    "SCHEMA_PATH_PREFIX": r"/api/",
+}

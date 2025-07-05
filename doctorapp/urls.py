68a4122e1319c2735ef665ec7b3fcaecfc80bfc9
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-07-05 17:53:08 -0500

    Add API authentication URL to urlpatterns for DRF integration

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -22,6 +22,7 @@ from bookings import urls as urls_bookings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),  # para usar la autenticación de DRF
     path('', include('docs.urls')),
     path('api/patients/', include(urls_patients)),
     path('api/doctors/', include(urls_doctors)),

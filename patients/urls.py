ed413b6e7041b06d3bb7e5cc3ab8532dc8ce7d32
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-07-09 16:33:02 -0500

    Update patient URL patterns to remove redundant 'patients/' prefix

--- patients/urls.py
+++ patients/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import patient, patient_detail
 
 urlpatterns = [
-  path("patients/", patient),
-  path("patients/<int:pk>/", patient_detail),
+  path("", patient),
+  path("<int:pk>/", patient_detail),
 ]
